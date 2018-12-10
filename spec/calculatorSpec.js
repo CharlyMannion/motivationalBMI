e3b334f33768db857ffaf5a496f9b384cfb3b62e
Charlotte Mannion <charlottemannion100@gmail.com>
2018-12-10 10:10:11 +0000

    adds function to calculate the BMI from the given height and weight

--- spec/calculatorSpec.js
+++ spec/calculatorSpec.js
@@ -15,15 +15,24 @@ describe("Calculator", function() {
 
     describe("the function to set the height value", function() {
         it("sets the height to the given value", function(){
-            calculator.setHeight(171);
-            expect(calculator.height).toEqual(171)
+            calculator.setHeight(1.71);
+            expect(calculator.height).toEqual(1.71)
         })
     })
 
     describe("the function to set the weight value", function() {
         it("sets the height to the given value", function(){
-            calculator.setWeight(40);
-            expect(calculator.weight).toEqual(40)
+            calculator.setWeight(42);
+            expect(calculator.weight).toEqual(42)
+        })
+    })
+
+    describe("the function to calculate the BMI", function() {
+        it("calculates the BMI from the height and weight", function() {
+            calculator.setHeight(1.71);
+            calculator.setWeight(42);
+            calculator.calculateBMI();
+            expect(calculator.BMI).toEqual('14.36')
         })
     })
 
--- src/calculator.js
+++ src/calculator.js
@@ -1,6 +1,7 @@
 function Calculator() {
     this.height = 0
     this.weight = 0
+    this.BMI = 0
 }
 
 Calculator.prototype.setHeight = function(givenHeight) {
@@ -9,4 +10,12 @@ Calculator.prototype.setHeight = function(givenHeight) {
 
 Calculator.prototype.setWeight = function(givenWeight) {
     this.weight = givenWeight
+}
+
+Calculator.prototype.calculateBMI = function() {
+    var heightSquared = Math.pow(this.height, 2)
+    console.log(heightSquared)
+    var fullBMI = this.weight / heightSquared
+    this.BMI = fullBMI.toFixed(2)
+    console.log(this.BMI)
 }
