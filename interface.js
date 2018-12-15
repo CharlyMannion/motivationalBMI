4a4f7bc0bada98dd4a5c2cfa399bf409fa15c320
Charlotte Mannion <charlottemannion100@gmail.com>
2018-12-15 14:15:07 +0000

    attempts to hook up interface to model

--- interface.js
+++ interface.js
@@ -1,17 +1,43 @@
+function UpdateUi(calculator){
+
+    $("#inputedHeight").text(calculator.height); 
+    $("#inputedWeight").text(calculator.weight);
+    $("#result").text(calculator.BMI);
+  
+  }
+
 $( document ).ready(function() {
     var calculator = new Calculator();
 
-    $("#submit").on('click', (function(){
+    $("#inputedHeight").text(calculator.height);
+    $("#inputedWeight").text(calculator.weight);
+    $("#result").text(calculator.BMI);
+
+    // $("#submitHeight").on('click', (function(){
+    //     var inputHeight = $('#formInputHeight').val();
+    //     calculator.setHeight(inputHeight);
+    //     $("#inputedHeight").text(calculator.height);
+    //     UpdateUi(calculator);
+    // }));
+
+    // $("#submitWeight").on('click', (function(){
+    //     var inputedWeight = $('#formInputWeight').val();
+    //     calculator.setWeight(inputWeight);
+    //     $("#inputedWeight").text(calculator.weight);
+    //     UpdateUi(calculator);
+    // }));
+
+    $("#calculateBMI").on('click', (function(){
         var inputHeight = $('#formInputHeight').val();
-        var inputWeight = $('#formInputWeight').val();
         calculator.setHeight(inputHeight);
+        $("#inputedHeight").text(calculator.height);
+        var inputedWeight = $('#formInputWeight').val();
         calculator.setWeight(inputWeight);
+        $("#inputedWeight").text(calculator.weight);
         calculator.calculateBMI();
         $("#result").text(calculator.BMI);
+        UpdateUi(calculator);
     }));
-
-    // $("#cerealOptions").hide();
-    // $("#cerealOptions").text(menu.displayCerealOptions());
 
     $('#resetPage').click(function() {
         location.reload();
