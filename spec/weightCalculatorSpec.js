d966cefe5b1d14b57d87fbda7b51c1f587643f26
Charlotte Mannion <charlottemannion100@gmail.com>
2018-12-17 11:39:38 +0000

    creates function which calculates a goal weight from a given height and BMI target

--- spec/weightCalculatorSpec.js
+++ spec/weightCalculatorSpec.js
@@ -13,28 +13,28 @@ describe("WeightCalculator", function() {
         })
     })
 
-    // describe("the function to set the height value", function() {
-    //     it("sets the height to the given value", function(){
-    //         calculator.setHeight(1.71);
-    //         expect(calculator.height).toEqual(1.71)
-    //     })
-    // })
+    describe("the function to set the height value", function() {
+        it("sets the height to the given value", function(){
+            weightCalculator.setHeight(1.71);
+            expect(weightCalculator.height).toEqual(1.71)
+        })
+    })
 
-    // describe("the function to set the weight value", function() {
-    //     it("sets the height to the given value", function(){
-    //         calculator.setWeight(42);
-    //         expect(calculator.weight).toEqual(42)
-    //     })
-    // })
+    describe("the function to set the BMI value", function() {
+        it("sets the BMI to the given value", function(){
+            weightCalculator.setBMI(15);
+            expect(weightCalculator.BMI).toEqual(15)
+        })
+    })
 
-    // describe("the function to calculate the BMI", function() {
-    //     it("calculates the BMI from the height and weight", function() {
-    //         calculator.setHeight(1.71);
-    //         calculator.setWeight(42);
-    //         calculator.calculateBMI();
-    //         expect(calculator.BMI).toEqual('14.36')
-    //     })
-    // })
+    describe("the function to calculate the weight", function() {
+        it("calculates the desired weight target from the height and the BMI", function() {
+            weightCalculator.setHeight(1.71);
+            weightCalculator.setBMI(15);
+            weightCalculator.calculateWeight();
+            expect(weightCalculator.weight).toEqual('43.86')
+        })
+    })
 
 
 })
--- src/weightCalculator.js
+++ src/weightCalculator.js
@@ -1,19 +1,20 @@
 function WeightCalculator() {
     this.height = 0
     this.BMI = 0
+    this.weight = 0
 }
 
-// Calculator.prototype.setHeight = function(givenHeight) {
-//     this.height = givenHeight
-// }
+WeightCalculator.prototype.setHeight = function(givenHeight) {
+    this.height = givenHeight
+}
 
-// Calculator.prototype.setWeight = function(givenWeight) {
-//     this.weight = givenWeight
-// }
+WeightCalculator.prototype.setBMI = function(givenBMI) {
+    this.BMI = givenBMI
+}
 
-// Calculator.prototype.calculateBMI = function() {
-//     var heightSquared = Math.pow(this.height, 2)
-//     var fullBMI = this.weight / heightSquared
-//     this.BMI = fullBMI.toFixed(2)
-//     return this.BMI
-// }
+WeightCalculator.prototype.calculateWeight = function() {
+    var heightSquared = Math.pow(this.height, 2)
+    var fullWeight = this.BMI * heightSquared
+    this.weight = fullWeight.toFixed(2)
+    return this.weight
+}
